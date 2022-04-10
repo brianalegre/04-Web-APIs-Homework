cf6d8ff0d6ff9eb663a04ae2b5ed6c499ec4bd08
Brian Alegre <brialegre@yahoo.com>
2022-04-09 22:15:59 -0700

    Updated basic html and css

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # 04-Web-APIs-Homework
+
+
+
+# Criteria
+Click Start
+- Timer starts and a question shows
+- Multiple choice
+
+Correct Answer
+- Go to next question
+
+Incorrect Answer
+- Minus time from timer
+- Next question
+
+All questions or Timer is 0
+- Game over
+
+Save High Score
+- Initial
+- Score
+
---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+// list of all questions, choices, and answers
+var questions = [
+{
+    title: "Who's that Pokemon?",
+    choices: ["Pikachu", "Bulbasaur", "Charmander", "Squirtle"],
+    answer: "Pikachu"
+},
+{
+    title: "Who's that Pokemon?",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "Charmander"
+},
+{
+    title: "Who's that Pokemon?",
+    choices: ["Pikachu", "Bulbasaur", "Charmander", "Squirtle"],
+    answer: "Squirtle"
+},
+];
