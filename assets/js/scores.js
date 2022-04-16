1a1b689624508fb1507d3da3d014828c0025def5
Brian Alegre <brialegre@yahoo.com>
2022-04-16 15:31:49 -0700

    Added additional comments in scripts

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,29 +1,32 @@
-function printHighscores() {
-  // either get scores from localstorage or set to empty array
+// Function for showing highsocres
+function displayHighscores() {
+  // Get highscrces from localstorage
   var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-  // sort highscores by score property in descending order
+  // Sort highscores
   highscores.sort(function(a, b) {
     return b.score - a.score;
   });
 
   highscores.forEach(function(score) {
-    // create li tag for each high score
+    // Create li for each score
     var liTag = document.createElement("li");
     liTag.textContent = score.initials + ": " + score.score;
 
-    // display on page
+    // Display score on page
     var olEl = document.getElementById("highscores");
     olEl.appendChild(liTag);
   });
 }
 
+// Function for clearing highscores
 function clearHighscores() {
   window.localStorage.removeItem("highscores");
   window.location.reload();
 }
 
+// Listen for click
 document.getElementById("clear").onclick = clearHighscores;
 
-// run function when page loads
-printHighscores();
+// Run function on page load
+displayHighscores();
